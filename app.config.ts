7cb1e42c053f2bd0ada8d3a092df238e720872cf
chlbri <bri_lvi@icloud.com>
2025-01-06 18:40:15 +0000

    feat:preprender

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from '@tanstack/start/config';
 export default defineConfig({
   server: {
     preset: 'vercel',
+    prerender: { routes: ['/', 'about', 'home'], crawlLinks: true },
   },
 });
