6b8477a462e9ffe9c600d5d59e15b5e3bb43495a
chlbri <bri_lvi@icloud.com>
2025-01-06 18:51:07 +0000

    fix(routing): prerender

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@tanstack/start/config';
 
 export default defineConfig({
   server: {
-    prerender: { routes: ['/', '/about', '/home'], crawlLinks: true },
+    prerender: { routes: ['/', '/about', '/other'], crawlLinks: true },
   },
 });
