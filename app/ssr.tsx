fd5ebaa1d586ac7dfb96d19ccb9b94cee392956f
chlbri <bri_lvi@icloud.com>
2025-01-06 18:28:44 +0000

    ok

--- app/ssr.tsx
+++ app/ssr.tsx
@@ -1,13 +1,11 @@
 // app/ssr.tsx
 /// <reference types="vinxi/types/server" />
-import { getRouterManifest } from '@tanstack/start/router-manifest';
 import {
   createStartHandler,
   defaultStreamHandler,
 } from '@tanstack/start/server';
 
 import { createRouter } from './router';
-getRouterManifest;
 
 export default createStartHandler({
   createRouter,
